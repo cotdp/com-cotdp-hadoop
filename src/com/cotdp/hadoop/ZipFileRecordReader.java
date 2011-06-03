8d5fc18836a68c3e8a3b072d1e2b7e28ed25b0be
Michael Cutler <m@cotdp.com>
2011-06-03 23:58:38 +0100

    Corrected whitespace & indentation

--- src/com/cotdp/hadoop/ZipFileRecordReader.java
+++ src/com/cotdp/hadoop/ZipFileRecordReader.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *	 http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -44,10 +44,10 @@ public class ZipFileRecordReader extends RecordReader<Text,BytesWritable>
 	public void initialize(InputSplit inputSplit, TaskAttemptContext taskAttemptContext) throws IOException, InterruptedException
 	{
 		FileSplit split = (FileSplit) inputSplit;
-        Configuration conf = taskAttemptContext.getConfiguration();
-        Path path = split.getPath();
-        FileSystem fs = path.getFileSystem(conf);
-        fsin = fs.open(path);
+		Configuration conf = taskAttemptContext.getConfiguration();
+		Path path = split.getPath();
+		FileSystem fs = path.getFileSystem(conf);
+		fsin = fs.open(path);
 		zip = new ZipInputStream(fsin);
 	}
 	
